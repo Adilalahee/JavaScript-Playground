95fb5f45cb3ec5d9da7829794dfc52087624c145
Adil Alahee <adilmdctg@gmail.com>
2025-06-12 20:20:02 +0600

    javascript 1 to 15

---
+++ LWS/index.js
@@ -0,0 +1,57 @@
+//datatypes
+// let x=5
+// let y="adil"
+// let z=5.3
+// let w=true
+// const a=['adil',"akil"]
+// const b={"name":"adil","age":"23"}
+// let c
+// let d=""
+// console.log(typeof x)
+// console.log(typeof y)
+// console.log(typeof z)
+// console.log(typeof w)
+// console.log(typeof a)
+// console.log(typeof b)
+// console.log(typeof c)
+// console.log(typeof d)
+
+//function
+// function sleep() {
+//     console.log("This is adil");
+// }
+// sleep()
+// sleep()
+// function woke(name) {
+//     console.log(name+ "This is adil");
+// }
+// woke("adil")
+
+
+//Object
+// const car={
+//     name:"bmw",
+//     color:"blue",
+//     weight:499,
+//     start:function (time){
+//         this.drive()
+//         console.log("car has started"+time);   
+//     },
+//     drive:function () {
+//         console.log("Car is driving now");
+        
+//     }
+// }
+// console.log(car.color)
+// console.log(car['weight']);
+// car.start("5pm")
+
+// let x=5
+// const y=new Number(5)
+
+// console.log(typeof x);
+// console.log(typeof y);
+// console.log(y);
+
+
+
